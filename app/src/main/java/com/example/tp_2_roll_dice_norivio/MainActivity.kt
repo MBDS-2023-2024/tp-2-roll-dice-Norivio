62ddf3257ad0fb6d3e5e3c177ba3051d7995c7a3
Ivionel Norgaisse <inorgaisse@solutions.ht>
2024-07-25 09:31:15 -0400

    Ajouter la logique du lancer de des

--- app/src/main/java/com/example/tp_2_roll_dice_norivio/MainActivity.kt
+++ app/src/main/java/com/example/tp_2_roll_dice_norivio/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.tp_2_roll_dice_norivio
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+        val rollButton: Button = findViewById(R.id.btRoll)
+
+        rollButton.setOnClickListener {
+            /*val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            val resultDice:TextView = findViewById(R.id.txtDice)
+            resultDice.text = "6"*/
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.txtDice)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int){
+    fun roll(): Int{
+        return (1..numSides).random()
     }
 }
