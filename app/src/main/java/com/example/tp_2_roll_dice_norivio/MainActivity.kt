7ee77fa0f8b6eb6fc95ed9d5c3ec3d8c49d0e675
Ivionel Norgaisse <inorgaisse@solutions.ht>
2024-07-25 16:06:27 -0400

    Transformer l'application en Casino

--- app/src/main/java/com/example/tp_2_roll_dice_norivio/MainActivity.kt
+++ app/src/main/java/com/example/tp_2_roll_dice_norivio/MainActivity.kt
@@ -3,34 +3,44 @@ package com.example.tp_2_roll_dice_norivio
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
         val rollButton: Button = findViewById(R.id.btRoll)
 
-        rollButton.setOnClickListener {
-            /*val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            val resultDice:TextView = findViewById(R.id.txtDice)
-            resultDice.text = "6"*/
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
+        val dice2 = Dice(6)
+
         val diceRoll = dice.roll()
+        val diceRoll2 = dice2.roll()
+
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.txtDice)
         resultTextView.text = diceRoll.toString()
+
+        var resulTextView2: TextView = findViewById(R.id.txtDice2)
+        resulTextView2.text = diceRoll2.toString()
+
+        if(diceRoll == diceRoll2){
+            val toast = Toast.makeText(this, "Félicitation! Vous avez gagné", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
 
